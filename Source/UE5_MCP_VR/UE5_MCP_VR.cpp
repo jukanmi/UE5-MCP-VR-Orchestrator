2db4e2435b73659492f59024775feb9c4ad6c064
jukanmi <trailer@hanyang.ac.kr>
2026-01-17 21:15:40 +0900

    feat: initialize new Unreal Engine 5 VR project with core files

---
+++ Source/UE5_MCP_VR/UE5_MCP_VR.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_MCP_VR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_MCP_VR, "UE5_MCP_VR" );
